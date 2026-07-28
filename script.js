0cb3ac29221281d31f8b28e2a385ecca49488647
Yves Esteves <yves.yan@hotmail.com>
2026-07-28 16:54:16 -0300

    update - new sections, new visual, new effects, etc

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ function enviarWhats(event){
 
     window.open(url, '_blank')
 }
+
+// 1. EFEITO DE APARECER AO ROLAR (SCROLL REVEAL)
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('mostrar');
+        }
+    });
+}, {
+    threshold: 0.15 
+});
+
+const elementosParaAnimar = document.querySelectorAll('.secao-padrao, .contatos-secao-neon, .projetos-card, .stack-icons, .stack-bars');
+
+elementosParaAnimar.forEach((el) => {
+    el.classList.add('escondido');
+    observer.observe(el);
+});
+
+
+// 2. EFEITO DE PARTÍCULAS FLUTUANTES
+function criarParticulas() {
+    const container = document.querySelector('#bolinhas-container');
+    
+    // Cria 40 bolinhas
+    for (let i = 0; i < 40; i++) {
+        let particula = document.createElement('div');
+        particula.classList.add('ponto-flutuante');
+        
+        particula.style.left = Math.random() * 100 + 'vw';
+        particula.style.top = Math.random() * 100 + 'vh';
+        
+        particula.style.animationDuration = (Math.random() * 10 + 10) + 's'; 
+        particula.style.animationDelay = (Math.random() * 5) + 's';
+        
+        container.appendChild(particula);
+    }
+}
+
+criarParticulas();
